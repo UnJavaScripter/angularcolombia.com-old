4479eee3a61fa02215bf685b30d25232efdbd111
unJavaScripter <diegocoyt@gmail.com>
2017-06-29 12:08:39 -0500

    feat: agregar autenticacion con firebase
    closes #6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from '../environments/environment';
 
 import { RoutingModule } from './routing.module';
+import { SharedModule } from "./shared/shared.module";
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
@@ -19,7 +20,8 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     RoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
