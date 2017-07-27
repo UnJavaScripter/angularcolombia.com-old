2ffd5d2b77cd5ae3636fc915c6a059756d611e63
Carlos Andrés Charris S <darkklitos@gmail.com>
2017-07-26 22:02:30 -0500

    se crean metodos de logIn y logOut en el AuthService

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,43 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
-@Injectable()
+@Injectable ()
 export class AuthService {
-
-  constructor() { }
-
+  
+  user : firebase.User;
+  
+  constructor ( public afAuth : AngularFireAuth,
+                private router : Router ) {
+  }
+  
+  /**
+   * Metodo Login Google
+   * @author Carlos Andres
+   * @version 22/07/2017
+   */
+  login () {
+    this.afAuth.auth.signInWithPopup ( new firebase.auth.GoogleAuthProvider () )
+      .then ( ( data ) => {
+        this.user = data;
+        console.log ( this.user );
+      } );
+    // this.afAuth.auth.createUserWithEmailAndPassword(this.email, this.pass);
+  }
+  
+  /**
+   * Metodo de Logout
+   * @author Carlos Andres
+   * @version 22/07/2017
+   */
+  logout () {
+    this.afAuth.auth.signOut ();
+    this.user = undefined;
+    this.router.navigate ( [ 'home' ] );
+  }
+  
+  isLoggin () {
+    return ( !this.user ) ? false : true;
+  }
 }
